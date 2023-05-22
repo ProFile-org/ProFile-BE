a5d86f753dee67a78252aa657fca50c0caf5b26c
Nguyen Quang Chien <quangchieens1@gmail.com>
2023-05-22 15:54:36 +0700

    feat: get all departments

---
+++ tests/Application.Tests.Integration/Departments/Queries/GetAllDepartmentsTests.cs
@@ -0,0 +1,55 @@
+using Application.Common.Mappings;
+using Application.Departments.Queries.GetAllDepartments;
+using Application.Users.Queries;
+using AutoMapper;
+using Bogus;
+using Domain.Entities;
+using FluentAssertions;
+using Xunit;
+
+namespace Application.Tests.Integration.Departments.Queries;
+
+public class GetAllDepartmentsTests : BaseClassFixture
+{
+    private readonly IMapper _mapper;
+    public GetAllDepartmentsTests(CustomApiFactory apiFactory) : base(apiFactory)
+    {
+        var configuration = new MapperConfiguration(x => x.AddProfile<MappingProfile>());
+        _mapper = configuration.CreateMapper();
+    }
+
+    [Fact]
+    public async Task ShouldReturnDepartments_WhenDepartmentsExist()
+    {
+        // Arrange
+        var department = new Department()
+        {
+            Id = Guid.NewGuid(),
+            Name = new Faker().Commerce.Department()
+        };
+        await AddAsync(department);
+        var query = new GetAllDepartmentsQuery();
+
+        // Act
+        var result = await SendAsync(query);
+
+        // Assert
+        result.Should().ContainEquivalentOf(_mapper.Map<DepartmentDto>(department));
+        
+        // Cleanup
+        Remove(department);
+    }
+    
+    [Fact]
+    public async Task ShouldReturnEmptyList_WhenNoDepartmentsExist()
+    {
+        // Arrange
+        var query = new GetAllDepartmentsQuery();
+
+        // Act
+        var result = await SendAsync(query);
+
+        // Assert
+        result.Should().BeEmpty();
+    }
+}
