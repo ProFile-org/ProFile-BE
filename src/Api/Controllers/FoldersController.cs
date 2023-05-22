9a4cca00153adbde835a8784835cc615d46ca6ea
kaitoz11 <43519768+kaitoz11@users.noreply.github.com>
2023-05-23 00:18:15 +0700

    feat: add disable folder implementaiton

--- src/Api/Controllers/FoldersController.cs
+++ src/Api/Controllers/FoldersController.cs
@@ -1,6 +1,7 @@
 using Application.Common.Models;
 using Application.Common.Models.Dtos.Physical;
 using Application.Folders.Commands.AddFolder;
+using Application.Folders.Commands.DisableFolder;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Api.Controllers;
@@ -14,6 +15,13 @@ public class FoldersController : ApiControllerBase
     [ProducesResponseType(StatusCodes.Status404NotFound)]
     [ProducesResponseType(StatusCodes.Status409Conflict)]
     public async Task<ActionResult<Result<FolderDto>>> AddFolders([FromBody] AddFolderCommand command)
+    {
+        var result = await Mediator.Send(command);
+        return Ok(Result<FolderDto>.Succeed(result));
+    }
+
+    [HttpPost("disable")]
+    public async Task<ActionResult<Result<FolderDto>>> DisableFolder([FromBody] DisableFolderCommand command)
     {
         var result = await Mediator.Send(command);
         return Ok(Result<FolderDto>.Succeed(result));
---
+++ src/Application/Folders/Commands/DisableFolder/DisableFolderCommand.cs
@@ -0,0 +1,43 @@
+using Application.Common.Interfaces;
+using Application.Common.Models.Dtos.Physical;
+using AutoMapper;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Application.Folders.Commands.DisableFolder;
+
+public record DisableFolderCommand : IRequest<FolderDto>
+{
+    public Guid FolderId { get; init; }
+}
+
+public class DisableFolderCommandHandler : IRequestHandler<DisableFolderCommand, FolderDto>
+{
+    private readonly IApplicationDbContext _context;
+    private readonly IMapper _mapper;
+
+    public DisableFolderCommandHandler(IApplicationDbContext context, IMapper mapper)
+    {
+        _context = context;
+        _mapper = mapper;
+    }
+
+    public async Task<FolderDto> Handle(DisableFolderCommand request, CancellationToken cancellationToken)
+    {
+        var folder = await _context.Folders
+            .Include(f => f.Locker)
+            .FirstOrDefaultAsync(f => 
+                f.Id.Equals(request.FolderId) && f.IsAvailable,
+                cancellationToken);
+
+        if (folder is null)
+        {
+            throw new KeyNotFoundException("Folder does not exist.");
+        }
+
+        folder.IsAvailable = false;
+        _context.Folders.Update(folder);
+        await _context.SaveChangesAsync(cancellationToken);
+        return _mapper.Map<FolderDto>(folder);
+    }
+}
