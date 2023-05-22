157999f1b707136f07d84df141f7b85e712b6dc0
Nguyen Quang Chien <quangchieens1@gmail.com>
2023-05-22 15:32:38 +0700

    feat: get all document types

--- src/Application/Documents/Queries/GetDocumentTypes/GetDocumentTypesQuery.cs
+++ src/Application/Documents/Queries/GetDocumentTypes/GetDocumentTypesQuery.cs
@@ -1,5 +1,23 @@
+using System.Collections.ObjectModel;
+using Application.Common.Interfaces;
 using MediatR;
+using Microsoft.EntityFrameworkCore;
 
 namespace Application.Documents.Queries.GetDocumentTypes;
 
 public record GetDocumentTypesQuery : IRequest<IEnumerable<string>>;
+
+public class GetDocumentTypesQueryHandler : IRequestHandler<GetDocumentTypesQuery, IEnumerable<string>>
+{
+    private readonly IApplicationDbContext _context;
+
+    public GetDocumentTypesQueryHandler(IApplicationDbContext context)
+    {
+        _context = context;
+    }
+    public async Task<IEnumerable<string>> Handle(GetDocumentTypesQuery request, CancellationToken cancellationToken)
+    {
+        return new ReadOnlyCollection<string>(await _context.Documents.Select(x => x.DocumentType)
+            .ToListAsync(cancellationToken));
+    }
+} 
