03ed52b74f74a3a384bbe4fd467f997ace7d2f21
Vzart <vo.dang.khoa.main@gmail.com>
2023-05-23 23:15:57 +0700

    refactor: refactor test to be more readable

--- tests/Application.Tests.Integration/BaseClassFixture.cs
+++ tests/Application.Tests.Integration/BaseClassFixture.cs
@@ -1,12 +1,15 @@
 using Application.Departments.Commands.CreateDepartment;
+using Application.Helpers;
 using Bogus;
 using Domain.Common;
 using Domain.Entities;
+using Domain.Entities.Physical;
 using FluentAssertions;
 using Infrastructure.Persistence;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
+using NodaTime;
 using Xunit;
 
 namespace Application.Tests.Integration;
@@ -83,5 +86,108 @@ public class BaseClassFixture
         var context = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
 
         return await context.Set<TEntity>().CountAsync();
+    }
+
+    protected Department CreateDepartment()
+    {
+        
+        var department = new Department()
+        {
+            Id = Guid.NewGuid(),
+            Name = new Faker().Commerce.Department()
+        };
+
+        return department;
+    }
+
+    protected User CreateUser(Department department)
+    {
+        var user = new User()
+        {
+            Id = Guid.NewGuid(),
+            Department = department,
+            Role = new Faker().Name.JobType(),
+            Username = new Faker().Internet.UserName(),
+            Created = LocalDateTime.FromDateTime(DateTime.Now),
+            IsActivated = true,
+            IsActive = true,
+            PasswordHash = SecurityUtil.Hash(new Faker().Internet.Password())
+        };
+
+        return user;
+    }
+    
+    protected Document[] CreateNDocuments(int n)
+    {
+        var list = new List<Document>();
+        for (var i = 0; i < n; i++)
+        {
+            var document = new Document()
+            {
+                Id = Guid.NewGuid(),
+                Title = new Faker().Commerce.ProductName(),
+                DocumentType = "Something Something Type",
+            };
+            list.Add(document);
+        }
+
+        return list.ToArray();
+    }
+
+    protected Folder CreateFolder(params Document[] documents)
+    {
+        var folder = new Folder()
+        {
+            Id = Guid.NewGuid(),
+            Name = new Faker().Commerce.ProductName(),
+            Capacity = 3,
+            NumberOfDocuments = documents.Length,
+            IsAvailable = true
+        };
+
+        foreach (var document in documents)
+        {
+            folder.Documents.Add(document);
+        }
+
+        return folder;
+    }
+
+    protected Locker CreateLocker(params Folder[] folders)
+    {
+        var locker = new Locker()
+        {
+            Id = Guid.NewGuid(),
+            Name = new Faker().Commerce.ProductName(),
+            Capacity = 3,
+            NumberOfFolders = folders.Length,
+            IsAvailable = true
+        };
+
+        foreach (var folder in folders)
+        {
+            locker.Folders.Add(folder);
+        }
+
+        return locker;
+    }
+
+    protected Room CreateRoom(params Locker[] lockers)
+    {
+        var room = new Room()
+        {
+            Id = Guid.NewGuid(),
+            Name = new Faker().Commerce.ProductName(),
+            Capacity = 3,
+            NumberOfLockers = lockers.Length,
+            IsAvailable = true
+        };
+
+        foreach (var locker in lockers)
+        {
+            room.Lockers.Add(locker);
+        }
+
+        return room;
     }
 }
