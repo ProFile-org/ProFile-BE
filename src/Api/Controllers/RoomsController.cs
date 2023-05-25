828c6f5bfcbdd2844ac1e2b6e61f7db0a561b635
Vzart <vo.dang.khoa.main@gmail.com>
2023-05-25 23:47:57 +0700

    fix: add enpoint for disable room

--- src/Api/Controllers/RoomsController.cs
+++ src/Api/Controllers/RoomsController.cs
@@ -2,6 +2,7 @@ using Application.Common.Models;
 using Application.Common.Models.Dtos.Physical;
 using Application.Identity;
 using Application.Rooms.Commands.CreateRoom;
+using Application.Rooms.Commands.DisableRoom;
 using Application.Rooms.Commands.RemoveRoom;
 using Application.Rooms.Queries.GetEmptyContainersPaginated;
 using Infrastructure.Identity.Authorization;
@@ -31,6 +32,16 @@ public class RoomsController : ApiControllerBase
     {
         var result = await Mediator.Send(query);
         return Ok(Result<PaginatedList<EmptyLockerDto>>.Succeed(result));
+    }
+
+    [HttpPut]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status409Conflict)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public async Task<ActionResult<Result<RoomDto>>> DisableRoom(DisableRoomCommand command)
+    {
+        var result = await Mediator.Send(command);
+        return Ok(Result<RoomDto>.Succeed(result));
     }
 
     [HttpDelete]
