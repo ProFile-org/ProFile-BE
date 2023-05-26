97215f2f7c41a2203b86ab7d9a32a192a98384ab
Nguyen Quang Chien <quangchieens1@gmail.com>
2023-05-26 19:26:01 +0700

    add: remove locker endpoint and command

--- src/Api/Controllers/LockersController.cs
+++ src/Api/Controllers/LockersController.cs
@@ -4,6 +4,7 @@ using Application.Identity;
 using Application.Lockers.Commands.AddLocker;
 using Application.Lockers.Commands.DisableLocker;
 using Application.Lockers.Commands.EnableLocker;
+using Application.Lockers.Commands.RemoveLocker;
 using Infrastructure.Identity.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
@@ -33,6 +34,21 @@ public class LockersController : ApiControllerBase
     [HttpPut("enable")]
     public async Task<ActionResult<Result<LockerDto>>> EnableLocker([FromBody] EnableLockerCommand command)
     {
+        var result = await Mediator.Send(command);
+        return Ok(Result<LockerDto>.Succeed(result));
+    }
+    
+    [HttpDelete("{lockerId:guid}")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status403Forbidden)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [ProducesResponseType(StatusCodes.Status409Conflict)]
+    public async Task<ActionResult<Result<LockerDto>>> Remove([FromRoute] Guid lockerId)
+    {
+        var command = new RemoveLockerCommand()
+        {
+            LockerId = lockerId
+        };
         var result = await Mediator.Send(command);
         return Ok(Result<LockerDto>.Succeed(result));
     }
