92786ac18f4118c1181308d519d4501dcc184edb
Nguyen Quang Chien <quangchieens1@gmail.com>
2023-05-27 19:29:58 +0700

    add: remove staff from room command and endpoint

---
+++ src/Api/Controllers/Payload/Requests/Staffs/RemoveStaffFromRoomRequest.cs
@@ -0,0 +1,6 @@
+namespace Api.Controllers.Payload.Requests.Staffs;
+
+public class RemoveStaffFromRoomRequest
+{
+    public Guid RoomId { get; set; }
+}
--- src/Api/Controllers/StaffsController.cs
+++ src/Api/Controllers/StaffsController.cs
@@ -1,6 +1,8 @@
+using Api.Controllers.Payload.Requests.Staffs;
 using Application.Common.Models;
 using Application.Identity;
 using Application.Staffs.Commands.CreateStaff;
+using Application.Staffs.Commands.RemoveStaffFromRoom;
 using Application.Users.Queries.Physical;
 using Infrastructure.Identity.Authorization;
 using Microsoft.AspNetCore.Mvc;
@@ -16,6 +18,27 @@ public class StaffsController : ApiControllerBase
     [ProducesResponseType(StatusCodes.Status404NotFound)]
     public async Task<ActionResult<Result<StaffDto>>> CreateStaff([FromBody] CreateStaffCommand command)
     {
+        var result = await Mediator.Send(command);
+        return Ok(Result<StaffDto>.Succeed(result));
+    }
+    
+    /// <summary>
+    /// Remove a staff from room
+    /// </summary>
+    /// <param name="staffId">Id of the staff to be removed from room</param>
+    /// <param name="request">Remove details</param>
+    /// <returns>A StaffDto of the removed staff</returns>
+    [HttpPut("{staffId:guid}")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status403Forbidden)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public async Task<ActionResult<Result<StaffDto>>> RemoveStaffFromRoom([FromRoute] Guid staffId, [FromBody] RemoveStaffFromRoomRequest request)
+    {
+        var command = new RemoveStaffFromRoomCommand()
+        {
+            StaffId = staffId,
+            RoomId = request.RoomId,
+        };
         var result = await Mediator.Send(command);
         return Ok(Result<StaffDto>.Succeed(result));
     }
