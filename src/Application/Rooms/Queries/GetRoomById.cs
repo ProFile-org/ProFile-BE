01e57c34619d3d8d6fef3941ecaef4a6fb7097a8
Vzart <vo.dang.khoa.main@gmail.com>
2023-05-29 20:31:45 +0700

    feat: get room by id

--- src/Application/Rooms/Queries/GetRoomById.cs
+++ src/Application/Rooms/Queries/GetRoomById.cs
@@ -1,12 +1,51 @@
+using Application.Common.Interfaces;
 using Application.Common.Models.Dtos.Physical;
+using AutoMapper;
+using FluentValidation;
 using MediatR;
+using Microsoft.EntityFrameworkCore;
 
 namespace Application.Rooms.Queries;
 
 public class GetRoomById
 {
+    public class Validator : AbstractValidator<Query>
+    {
+        public Validator()
+        {
+            RuleLevelCascadeMode = CascadeMode.Stop;
+
+            RuleFor(x => x.RoomId)
+                .NotEmpty().WithMessage("RoomId is required.");
+        }   
+    }
     public record Query : IRequest<RoomDto>
     {
         public Guid RoomId { get; init; }
+    }
+    
+    public class GetRoomByIdHandler : IRequestHandler<Query, RoomDto>
+    {
+        private readonly IApplicationDbContext _context;
+        private readonly IMapper _mapper;
+
+        public GetRoomByIdHandler(IApplicationDbContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        public async Task<RoomDto> Handle(Query request, CancellationToken cancellationToken)
+        {
+            var room = await _context.Rooms
+                .FirstOrDefaultAsync(x => x.Id.Equals(request.RoomId), cancellationToken: cancellationToken);
+           
+            if (room is null)
+            {
+                throw new KeyNotFoundException("Room does not exist.");
+            }
+
+            return _mapper.Map<RoomDto>(room);
+        }
     }
 }
