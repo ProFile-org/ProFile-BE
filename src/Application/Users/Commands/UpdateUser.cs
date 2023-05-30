99d8042e3f724305b71a4ab8d2ba47431291fa57
Vzart <vo.dang.khoa.main@gmail.com>
2023-05-30 20:05:32 +0700

    feat: update user

--- src/Application/Users/Commands/UpdateUser.cs
+++ src/Application/Users/Commands/UpdateUser.cs
@@ -1,5 +1,8 @@
+using Application.Common.Interfaces;
 using Application.Users.Queries;
+using AutoMapper;
 using MediatR;
+using Microsoft.EntityFrameworkCore;
 
 namespace Application.Users.Commands;
 
@@ -11,5 +14,36 @@ public class UpdateUser
         public string? FirstName { get; init; }
         public string? LastName { get; init; }
         public string? Position { get; init; }
+    }
+    
+    public class CommandHandler : IRequestHandler<Command, UserDto>
+    {
+        private readonly IApplicationDbContext _context;
+        private readonly IMapper _mapper;
+
+        public CommandHandler(IApplicationDbContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        public async Task<UserDto> Handle(Command request, CancellationToken cancellationToken)
+        {
+            var user = await _context.Users
+                .FirstOrDefaultAsync(x => x.Id.Equals(request.UserId), cancellationToken: cancellationToken);
+
+            if (user is null)
+            {
+                throw new KeyNotFoundException("User does not exist.");
+            }
+
+            user.FirstName = request.FirstName;
+            user.LastName = request.LastName;
+            user.Position = request.Position;
+
+            var result = _context.Users.Update(user);
+            await _context.SaveChangesAsync(cancellationToken);
+            return _mapper.Map<UserDto>(result.Entity);
+        }
     }
 }
