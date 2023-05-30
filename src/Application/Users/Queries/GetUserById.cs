7466986a7eb78783c009f5caa2b61e5b1c58a409
Vzart <vo.dang.khoa.main@gmail.com>
2023-05-30 19:53:48 +0700

    feat: get user by id

--- src/Application/Users/Queries/GetUserById.cs
+++ src/Application/Users/Queries/GetUserById.cs
@@ -1,4 +1,7 @@
+using Application.Common.Interfaces;
+using AutoMapper;
 using MediatR;
+using Microsoft.EntityFrameworkCore;
 
 namespace Application.Users.Queries;
 
@@ -7,5 +10,30 @@ public class GetUserById
     public record Query : IRequest<UserDto>
     {
         public Guid UserId { get; init; }
+    }
+    
+    public class QueryHandler : IRequestHandler<Query, UserDto>
+    {
+        private readonly IApplicationDbContext _context;
+        private readonly IMapper _mapper;
+
+        public QueryHandler(IApplicationDbContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        public async Task<UserDto> Handle(Query request, CancellationToken cancellationToken)
+        {
+            var user = await _context.Users
+                .FirstOrDefaultAsync(x => x.Id.Equals(request.UserId), cancellationToken: cancellationToken);
+
+            if (user is null)
+            {
+                throw new KeyNotFoundException("User does not exist");
+            }
+
+            return _mapper.Map<UserDto>(user);
+        }
     }
 }
