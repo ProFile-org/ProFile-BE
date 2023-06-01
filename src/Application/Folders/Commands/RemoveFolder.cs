aad9bd6a308aeebbec9a1e03c1f6e6c838d1c78e
kaitoz11 <43519768+kaitoz11@users.noreply.github.com>
2023-06-01 15:15:57 +0700

    feat(RemoveFolder.cs): add remove folder feature

--- src/Application/Folders/Commands/RemoveFolder.cs
+++ src/Application/Folders/Commands/RemoveFolder.cs
@@ -1,12 +1,61 @@
+using Application.Common.Interfaces;
 using Application.Common.Models.Dtos.Physical;
+using AutoMapper;
+using FluentValidation;
 using MediatR;
+using Microsoft.EntityFrameworkCore;
 
 namespace Application.Folders.Commands;
 
 public class RemoveFolder
 {
+    public class Validator : AbstractValidator<Command>
+    {
+        public Validator()
+        {
+            RuleLevelCascadeMode = CascadeMode.Stop;
+
+            RuleFor(f => f.FolderId)
+                .NotEmpty().WithMessage("FolderId is required.");
+        }
+    }
+    
     public record Command : IRequest<FolderDto>
     {
         public Guid FolderId { get; init; }
+    }
+    
+    public class CommandHandler : IRequestHandler<Command, FolderDto>
+    {
+        private readonly IApplicationDbContext _context;
+        private readonly IMapper _mapper;
+
+        public CommandHandler(IApplicationDbContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        public async Task<FolderDto> Handle(Command request, CancellationToken cancellationToken)
+        {
+            var folder = await _context.Folders
+                .FirstOrDefaultAsync(x => x.Id.Equals(request.FolderId), cancellationToken);
+
+            if (folder is null)
+            {
+                throw new KeyNotFoundException("Folder does not exist.");
+            }
+
+            var containDocument = folder.NumberOfDocuments > 0;
+
+            if (containDocument)
+            {
+                throw new InvalidOperationException("Folder cannot be removed because it contains documents.");
+            }
+
+            var result = _context.Folders.Remove(folder);
+            await _context.SaveChangesAsync(cancellationToken);
+            return _mapper.Map<FolderDto>(result.Entity);
+        }
     }
 }
