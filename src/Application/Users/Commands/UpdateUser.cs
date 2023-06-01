7dc19a12c669e11daebc9b6d813090fcaf72dc99
Vzart <85790072+Vzart@users.noreply.github.com>
2023-06-01 19:21:12 +0700

    Feat/update user (#120)
    * add: integration test
    
    * feat: update user
    
    * refactor: add validator
    
    ---------
    
    Co-authored-by: Nguyen Quang Chien <quangchieens1@gmail.com>

--- src/Application/Users/Commands/UpdateUser.cs
+++ src/Application/Users/Commands/UpdateUser.cs
@@ -1,16 +1,66 @@
+using Application.Common.Interfaces;
 using Application.Users.Queries;
+using AutoMapper;
+using FluentValidation;
 using MediatR;
+using Microsoft.EntityFrameworkCore;
 
 namespace Application.Users.Commands;
 
 public class UpdateUser
 {
+    public class Validator : AbstractValidator<Command>
+    {
+        public Validator()
+        {
+            RuleLevelCascadeMode = CascadeMode.Stop;
+
+            RuleFor(x => x.FirstName)
+                .MaximumLength(50).WithMessage("FirstName can not exceed 50 characters.");
+
+            RuleFor(x => x.LastName)
+                .MaximumLength(50).WithMessage("LastName can not exceed 50 characters.");
+
+            RuleFor(x => x.Position)
+                .MaximumLength(64).WithMessage("Position can not exceed 64 characters.");
+        }
+    }
     public record Command : IRequest<UserDto>
     {
         public Guid UserId { get; init; }
         public string? FirstName { get; init; }
         public string? LastName { get; init; }
-        public string Role { get; init; } = null!;
         public string? Position { get; init; }
+    }
+    
+    public class CommandHandler : IRequestHandler<Command, UserDto>
+    {
+        private readonly IApplicationDbContext _context;
+        private readonly IMapper _mapper;
+
+        public CommandHandler(IApplicationDbContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        public async Task<UserDto> Handle(Command request, CancellationToken cancellationToken)
+        {
+            var user = await _context.Users
+                .FirstOrDefaultAsync(x => x.Id.Equals(request.UserId), cancellationToken: cancellationToken);
+
+            if (user is null)
+            {
+                throw new KeyNotFoundException("User does not exist.");
+            }
+
+            user.FirstName = request.FirstName;
+            user.LastName = request.LastName;
+            user.Position = request.Position;
+
+            var result = _context.Users.Update(user);
+            await _context.SaveChangesAsync(cancellationToken);
+            return _mapper.Map<UserDto>(result.Entity);
+        }
     }
 }
