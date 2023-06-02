5d0b657b9de15e62fc19a2be7d0ae451c15a364f
kaitozu <43519768+kaitoz11@users.noreply.github.com>
2023-06-02 22:45:55 +0700

    Feat/delete document (#163)
    * add: integration test
    
    * feat: add delete document feature
    
    * refactor: remove unnecessary check ?
    
    * I hate this shitgit add .! Plss refactor those testsgit add .!
    
    ---------
    
    Co-authored-by: Nguyen Quang Chien <quangchieens1@gmail.com>

--- src/Application/Documents/Commands/DeleteDocument.cs
+++ src/Application/Documents/Commands/DeleteDocument.cs
@@ -1,5 +1,8 @@
+using Application.Common.Interfaces;
 using Application.Common.Models.Dtos.Physical;
+using AutoMapper;
 using MediatR;
+using Microsoft.EntityFrameworkCore;
 
 namespace Application.Documents.Commands;
 
@@ -8,5 +11,41 @@ public class DeleteDocument
     public record Command : IRequest<DocumentDto>
     {
         public Guid DocumentId { get; init; }
+    }
+    
+    public class CommandHandler : IRequestHandler<Command, DocumentDto>
+    {
+        private readonly IApplicationDbContext _context;
+        private readonly IMapper _mapper;
+
+        public CommandHandler(IApplicationDbContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        public async Task<DocumentDto> Handle(Command request, CancellationToken cancellationToken)
+        {
+            var document = await _context.Documents
+                .Include( x => x.Folder)
+                .FirstOrDefaultAsync(x => x.Id.Equals(request.DocumentId), cancellationToken);
+
+            if (document is null)
+            {
+                throw new KeyNotFoundException("Document does not exist.");
+            }
+
+            var folder = document.Folder;
+            var result = _context.Documents.Remove(document);
+
+            if (folder is not null)
+            {
+                folder.NumberOfDocuments -= 1;
+                _context.Folders.Update(folder);
+            }
+            
+            await _context.SaveChangesAsync(cancellationToken);
+            return _mapper.Map<DocumentDto>(result.Entity);
+        }
     }
 }
