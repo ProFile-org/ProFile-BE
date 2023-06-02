fff7635094565f867491ffd97fc4d611e974dc60
Vzart <vo.dang.khoa.main@gmail.com>
2023-06-02 11:27:33 +0700

    add: implementation for staff- not done

--- src/Application/Staffs/Queries/GetAllStaffsPaginated.cs
+++ src/Application/Staffs/Queries/GetAllStaffsPaginated.cs
@@ -1,6 +1,12 @@
+using Application.Common.Extensions;
+using Application.Common.Interfaces;
+using Application.Common.Mappings;
 using Application.Common.Models;
 using Application.Common.Models.Dtos.Physical;
+using AutoMapper;
+using AutoMapper.QueryableExtensions;
 using MediatR;
+using Microsoft.EntityFrameworkCore;
 
 namespace Application.Staffs.Queries;
 
@@ -13,5 +19,44 @@ public class GetAllStaffsPaginated
         public int? Size { get; set; }
         public string? SortBy { get; set; }
         public string? SortOrder { get; set; }
+    }
+    
+    public class QueryHandler : IRequestHandler<Query, PaginatedList<StaffDto>>
+    {
+        private readonly IApplicationDbContext _context;
+        private readonly IMapper _mapper;
+
+        public QueryHandler(IApplicationDbContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        public async Task<PaginatedList<StaffDto>> Handle(Query request, CancellationToken cancellationToken)
+        {
+            var staffs = _context.Staffs.Include(x => x.User).AsQueryable();
+            
+            if (!(request.SearchTerm is null || request.SearchTerm.Trim().Equals(string.Empty)))
+            {
+               staffs = staffs.Where(x => x.User.Username.ToLower().Contains(request.SearchTerm.ToLower()));
+            }
+
+            var sortBy = request.SortBy;
+            if (sortBy is null || !sortBy.MatchesPropertyName<StaffDto>())
+            {
+                sortBy = nameof(StaffDto.User.Id);
+            }
+            
+            var sortOrder = request.SortOrder ?? "asc";
+            var pageNumber = request.Page is null or <= 0 ? 1 : request.Page;
+            var sizeNumber = request.Size is null or <= 0 ? 5 : request.Size;
+
+            var result = await staffs
+                .ProjectTo<StaffDto>(_mapper.ConfigurationProvider)
+                .OrderByCustom(sortBy, sortOrder)
+                .PaginatedListAsync(pageNumber.Value, sizeNumber.Value);
+
+            return result;
+        }
     }
 }
