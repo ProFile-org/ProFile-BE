d66ba0ee757bffdec3af196e27a73244b0431fcd
Vzart <85790072+Vzart@users.noreply.github.com>
2023-06-03 22:12:05 +0700

    Feat/get all staffs paginated + tests (#165)
    * add: implementation for staff- not done
    
    * fix: get all paginated work but wait for pagination bug to merge
    
    * feat: get all staff paginated + tests
    
    * fix: pagination order

--- src/Application/Common/Models/Dtos/Physical/StaffDto.cs
+++ src/Application/Common/Models/Dtos/Physical/StaffDto.cs
@@ -1,11 +1,20 @@
 using Application.Common.Mappings;
 using Application.Users.Queries;
+using AutoMapper;
 using Domain.Entities.Physical;
 
 namespace Application.Common.Models.Dtos.Physical;
 
 public class StaffDto : IMapFrom<Staff>
 {
+    public Guid Id { get; set; }
     public UserDto User { get; set; } = null!;
     public RoomDto? Room { get; set; }
+    
+    public void Mapping(Profile profile)
+    {
+        profile.CreateMap<Staff, StaffDto>()
+            .ForMember(dest => dest.Id,
+                opt => opt.MapFrom(src => src.User.Id));
+    }
 }
--- src/Application/Staffs/Queries/GetAllStaffsPaginated.cs
+++ src/Application/Staffs/Queries/GetAllStaffsPaginated.cs
@@ -1,6 +1,12 @@
+using Application.Common.Extensions;
+using Application.Common.Interfaces;
+using Application.Common.Mappings;
 using Application.Common.Models;
 using Application.Common.Models.Dtos.Physical;
+using AutoMapper;
+using AutoMapper.QueryableExtensions;
 using MediatR;
+using Microsoft.EntityFrameworkCore;
 
 namespace Application.Staffs.Queries;
 
@@ -13,5 +19,50 @@ public class GetAllStaffsPaginated
         public int? Size { get; set; }
         public string? SortBy { get; set; }
         public string? SortOrder { get; set; }
+    }
+    
+    public class QueryHandler : IRequestHandler<Query, PaginatedList<StaffDto>>
+    {
+        private readonly IApplicationDbContext _context;
+        private readonly IMapper _mapper;
+
+        public QueryHandler(IApplicationDbContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        public async Task<PaginatedList<StaffDto>> Handle(Query request, CancellationToken cancellationToken)
+        {
+            var staffs = _context.Staffs
+                .Include(x => x.Room)
+                .Include(x => x.User)
+                .ThenInclude(x => x.Department)
+                .AsQueryable();
+            
+            if (!(request.SearchTerm is null || request.SearchTerm.Trim().Equals(string.Empty)))
+            {
+               staffs = staffs.Where(x => x.User.Username.ToLower().Contains(request.SearchTerm.ToLower()));
+            }
+
+            var sortBy = request.SortBy;
+            if (sortBy is null || !sortBy.MatchesPropertyName<StaffDto>())
+            {
+                sortBy = nameof(StaffDto.Id);
+            }
+            
+            var sortOrder = request.SortOrder ?? "asc";
+            var pageNumber = request.Page is null or <= 0 ? 1 : request.Page;
+            var sizeNumber = request.Size is null or <= 0 ? 5 : request.Size;
+            
+            var list = await staffs
+                .OrderByCustom(sortBy, sortOrder)
+                .Paginate(pageNumber.Value,sizeNumber.Value)
+                .ToListAsync(cancellationToken);
+
+            var result = _mapper.Map<List<StaffDto>>(list);
+            
+            return new PaginatedList<StaffDto>(result, result.Count, pageNumber.Value, sizeNumber.Value);
+        }
     }
 }
