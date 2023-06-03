f0b6ae6508cc3c711596af02b2fd4c5ca7a04384
StarryFolf <dangbao2003lqd@gmail.com>
2023-06-03 21:30:41 +0700

    fix: added room checking

--- tests/Application.Tests.Integration/Lockers/Commands/UpdateLockerTests.cs
+++ tests/Application.Tests.Integration/Lockers/Commands/UpdateLockerTests.cs
@@ -129,4 +129,43 @@ public class UpdateLockerTests : BaseClassFixture
         Remove(room);
         Remove(await FindAsync<Department>(department.Id));
     }
+
+    [Fact]
+    public async Task ShouldUpdateLocker_WhenSameNameExistsButInDifferentRoom()
+    {
+        // Arrange
+        var department1 = CreateDepartment();
+        var department2 = CreateDepartment();
+        var locker = CreateLocker();
+        var duplicateNameLocker = CreateLocker();
+        var room1 = CreateRoom(department1, locker);
+        var room2 = CreateRoom(department2, duplicateNameLocker);
+        await AddAsync(room1);
+        await AddAsync(room2);
+
+        var command = new UpdateLocker.Command()
+        {
+            LockerId = locker.Id,
+            Name = duplicateNameLocker.Name,
+            Capacity = 23,
+            Description = "fuck",
+        };
+        
+        // Act
+        var result = await SendAsync(command);
+        
+        // Assert
+        result.Id.Should().Be(locker.Id);
+        result.Name.Should().Be(command.Name);
+        result.Capacity.Should().Be(command.Capacity);
+        result.Description.Should().Be(command.Description);
+        
+        // Cleanup
+        Remove(locker);
+        Remove(duplicateNameLocker);
+        Remove(room1);
+        Remove(room2);
+        Remove(await FindAsync<Department>(department1.Id));
+        Remove(await FindAsync<Department>(department2.Id));
+    }
 }
