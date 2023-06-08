06a67d8c03b769d80160e85f23b52b420f6a31e9
StarryFolf <dangbao2003lqd@gmail.com>
2023-06-08 12:28:35 +0700

    feat: implement update user group test: add integration tests for update user group

---
+++ src/Application/UserGroups/Commands/UpdateUserGroup.cs
@@ -0,0 +1,63 @@
+﻿using Application.Common.Exceptions;
+using Application.Common.Interfaces;
+using Application.Common.Models.Dtos.Digital;
+using AutoMapper;
+using Domain.Entities.Digital;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Application.UserGroups.Commands;
+
+public class UpdateUserGroup
+{
+    public record Command : IRequest<UserGroupDto>
+    {
+        public Guid UserGroupId { get; init; }
+        public string Name { get; init; } = null!;
+    }
+
+    public class CommandHandler : IRequestHandler<Command, UserGroupDto>
+    {
+        private readonly IApplicationDbContext _context;
+        private readonly IMapper _mapper;
+
+        public CommandHandler(IApplicationDbContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        public async Task<UserGroupDto> Handle(Command request, CancellationToken cancellationToken)
+        {
+            var userGroup = await _context.UserGroups
+                .FirstOrDefaultAsync(x => x.Id == request.UserGroupId, cancellationToken);
+
+            if (userGroup is null)
+            {
+                throw new KeyNotFoundException("User group does not exist.");
+            }
+
+            var existedUserGroup = await _context.UserGroups
+                .FirstOrDefaultAsync(x => x.Name.Equals(request.Name) 
+                                          && x.Id != userGroup.Id, cancellationToken);
+
+            if (existedUserGroup is not null)
+            {
+                throw new ConflictException("New user group name already exists.");
+            }
+
+            var updatedUserGroup = new UserGroup()
+            {
+                Id = userGroup.Id,
+                Name = request.Name,
+                Users = userGroup.Users,
+            };
+            
+            _context.UserGroups.Entry(userGroup).State = EntityState.Detached;
+            _context.UserGroups.Entry(updatedUserGroup).State = EntityState.Modified;
+            
+            await _context.SaveChangesAsync(cancellationToken);
+            return _mapper.Map<UserGroupDto>(updatedUserGroup);
+        }
+    }
+}
---
+++ tests/Application.Tests.Integration/UserGroups/Commands/UpdateUserGroupTests.cs
@@ -0,0 +1,87 @@
+﻿using Application.Common.Exceptions;
+using Application.UserGroups.Commands;
+using Domain.Entities;
+using Domain.Entities.Digital;
+using FluentAssertions;
+using Infrastructure.Persistence;
+using Microsoft.Extensions.DependencyInjection;
+using Xunit;
+
+namespace Application.Tests.Integration.UserGroups.Commands;
+
+public class UpdateUserGroupTests : BaseClassFixture
+{
+    public UpdateUserGroupTests(CustomApiFactory apiFactory) : base(apiFactory)
+    {
+        
+    }
+
+    [Fact]
+    public async Task ShouldUpdateLocker_WhenDetailsAreValid()
+    {
+        // Arrange
+        var userGroup = CreateUserGroup(Array.Empty<User>());
+        await AddAsync(userGroup);
+
+        var command = new UpdateUserGroup.Command()
+        {
+            UserGroupId = userGroup.Id,
+            Name = "Updated name",
+        };
+        
+        // Act
+        var result = await SendAsync(command);
+        
+        // Assert
+        result.Name.Should().Be(command.Name);
+        
+        // Cleanup
+        Remove(await FindAsync<UserGroup>(userGroup.Id));
+    }
+
+    [Fact]
+    public async Task ShouldThrowKeyNotFoundException_WhenUserGroupDoesNotExist()
+    {
+        // Arrange
+        var command = new UpdateUserGroup.Command()
+        {
+            UserGroupId = Guid.NewGuid(),
+            Name = "Name",
+        };
+        
+        // Act
+        var result = async () => await SendAsync(command);
+        
+        // Assert
+        await result.Should().ThrowAsync<KeyNotFoundException>()
+            .WithMessage("User group does not exist.");
+    }
+
+    [Fact]
+    public async Task ShouldThrowConflictException_WhenNewUserGroupNameAlreadyExists()
+    {
+        // Arrange
+        var userGroup = CreateUserGroup(Array.Empty<User>());
+        await AddAsync(userGroup);
+        
+        var updateUserGroup = CreateUserGroup(Array.Empty<User>());
+        await AddAsync(updateUserGroup);
+
+        var command = new UpdateUserGroup.Command()
+        {
+            UserGroupId = updateUserGroup.Id,
+            Name = userGroup.Name,
+        };
+        
+        // Act
+        var result = async () => await SendAsync(command);
+        
+        // Assert
+        await result.Should().ThrowAsync<ConflictException>()
+            .WithMessage("New user group name already exists.");
+        
+        // Cleanup
+        Remove(userGroup);
+        Remove(updateUserGroup);
+    }
+}
