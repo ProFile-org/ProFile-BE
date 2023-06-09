18070fda6d4435ad61fde847f5db5649f2c06baa
StarryFolf <dangbao2003lqd@gmail.com>
2023-06-09 08:42:51 +0700

    feat: implement get user groups test: added integration tests for getting user groups

---
+++ src/Application/UserGroups/Queries/GetAllUserGroups.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.ObjectModel;
+using Application.Common.Interfaces;
+using Application.Common.Models.Dtos.Digital;
+using AutoMapper;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Application.UserGroups.Queries;
+
+public class GetAllUserGroups
+{
+    public record Query : IRequest<IEnumerable<UserGroupDto>>;
+
+    public class QueryHandler : IRequestHandler<Query, IEnumerable<UserGroupDto>>
+    {
+        private readonly IApplicationDbContext _context;
+        private readonly IMapper _mapper;
+
+        public QueryHandler(IApplicationDbContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        public async Task<IEnumerable<UserGroupDto>> Handle(Query request, CancellationToken cancellationToken)
+        {
+            var userGroups = await _context.UserGroups
+                .ToListAsync(cancellationToken);
+            var result = new ReadOnlyCollection<UserGroupDto>(_mapper.Map<List<UserGroupDto>>(userGroups));
+            return result;
+        }
+    } 
+}
---
+++ tests/Application.Tests.Integration/UserGroups/Queries/GetAllUserGroupsTests.cs
@@ -0,0 +1,51 @@
+﻿using Application.Common.Mappings;
+using Application.Common.Models.Dtos.Digital;
+using Application.UserGroups.Queries;
+using AutoMapper;
+using Domain.Entities;
+using FluentAssertions;
+using Xunit;
+
+namespace Application.Tests.Integration.UserGroups.Queries;
+
+public class GetAllUserGroupsTests : BaseClassFixture
+{
+    private readonly IMapper _mapper;
+    public GetAllUserGroupsTests(CustomApiFactory apiFactory) : base(apiFactory)
+    {
+        var configuration = new MapperConfiguration(x => x.AddProfile<MappingProfile>());
+        _mapper = configuration.CreateMapper();
+    }
+
+    [Fact]
+    public async Task ShouldReturnUserGroups_WhenUserGroupsExist()
+    {
+        // Arrange
+        var userGroup = CreateUserGroup(Array.Empty<User>());
+        
+        await AddAsync(userGroup);
+        var query = new GetAllUserGroups.Query();
+
+        // Act
+        var result = await SendAsync(query);
+
+        // Assert
+        result.Should().ContainEquivalentOf(_mapper.Map<UserGroupDto>(userGroup));
+        
+        // Cleanup
+        Remove(userGroup);
+    }
+    
+    [Fact]
+    public async Task ShouldReturnEmptyList_WhenNoUserGroupsExist()
+    {
+        // Arrange
+        var query = new GetAllUserGroups.Query();
+
+        // Act
+        var result = await SendAsync(query);
+
+        // Assert
+        result.Count().Should().Be(0);
+    }
+}
