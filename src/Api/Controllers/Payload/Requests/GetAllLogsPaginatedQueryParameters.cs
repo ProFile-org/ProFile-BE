ca8879e3517d5b25cf766b9c2209a249c300da77
Vzart <vo.dang.khoa.main@gmail.com>
2023-06-14 00:14:20 +0700

    get document log by id + get document logs paginated

---
+++ src/Api/Controllers/Payload/Requests/GetAllLogsPaginatedQueryParameters.cs
@@ -0,0 +1,9 @@
+﻿namespace Api.Controllers.Payload.Requests;
+
+/// <summary>
+///  get all logs paginated
+/// </summary>
+public class GetAllLogsPaginatedQueryParameters : PaginatedQueryParameters
+{
+    public string? SearchTerm { get; set; }    
+}
