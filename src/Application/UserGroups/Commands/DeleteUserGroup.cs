6f980e1c8e5ce9dc9e28e527cf4cd6e7f4d8e7dd
StarryFolf <67864500+StarryFolf@users.noreply.github.com>
2023-06-22 22:34:10 +0700

    feat: implement delete user group (#222)
    * feat: implement delete user group
    test: add integration tests for deleting user group
    
    * Update DeleteUserGroup.cs
    
    * Update DeleteUserGroupTests.cs

---
+++ src/Application/UserGroups/Commands/DeleteUserGroup.cs
@@ -0,0 +1,41 @@
+﻿using Application.Common.Interfaces;
+using Application.Common.Models.Dtos.Digital;
+using AutoMapper;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Application.UserGroups.Commands;
+
+public class DeleteUserGroup
+{
+    public record Command : IRequest<UserGroupDto>
+    {
+        public Guid UserGroupId { get; init; }
+    }
+    
+    public class CommandHandler : IRequestHandler<Command, UserGroupDto>
+    {
+        private readonly IApplicationDbContext _context;
+        private readonly IMapper _mapper;
+
+        public CommandHandler(IApplicationDbContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+        
+        public async Task<UserGroupDto> Handle(Command request, CancellationToken cancellationToken)
+        {
+            var userGroup = await _context.UserGroups.FirstOrDefaultAsync(x => x.Id == request.UserGroupId, cancellationToken);
+
+            if (userGroup is null)
+            {
+                throw new KeyNotFoundException("User group does not exist.");
+            }
+
+            var result = _context.UserGroups.Remove(userGroup);
+            await _context.SaveChangesAsync(cancellationToken);
+            return _mapper.Map<UserGroupDto>(result.Entity);
+        }
+    }
+}
