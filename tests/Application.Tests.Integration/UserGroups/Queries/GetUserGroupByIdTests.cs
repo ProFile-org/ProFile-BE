e7031a613ac882ad93de1b66f59b25be633dcfb5
StarryFolf <67864500+StarryFolf@users.noreply.github.com>
2023-06-23 15:38:23 +0700

    feat: implement get user group (#225)
    * feat: implement get user groups
    test: added integration tests for getting user groups
    
    * naming
    
    * refactor: made get all to be paginated and added equivalent tests

---
+++ tests/Application.Tests.Integration/UserGroups/Queries/GetUserGroupByIdTests.cs
@@ -0,0 +1,53 @@
+﻿using Application.UserGroups.Queries;
+using Domain.Entities;
+using FluentAssertions;
+using Xunit;
+
+namespace Application.Tests.Integration.UserGroups.Queries;
+
+public class GetUserGroupByIdTests : BaseClassFixture
+{
+    public GetUserGroupByIdTests(CustomApiFactory apiFactory) : base(apiFactory)
+    {
+    }
+
+    [Fact]
+    public async Task ShouldReturnUserGroup_WhenThatUserGroupExists()
+    {
+        // Arrange
+        var userGroup = CreateUserGroup(Array.Empty<User>());
+
+        await AddAsync(userGroup);
+
+        var query = new GetUserGroupById.Query()
+        {
+            UserGroupId = userGroup.Id,
+        };
+        
+        // Act
+        var result = await SendAsync(query);
+        
+        // Assert
+        result.Name.Should().Be(userGroup.Name);
+        
+        // Cleanup
+        Remove(userGroup);
+    }
+
+    [Fact]
+    public async Task ShouldThrowKeyNotFoundException_WhenUserGroupDoesNotExist()
+    {
+        // Arrange
+        var query = new GetUserGroupById.Query()
+        {
+            UserGroupId = Guid.NewGuid(),
+        };
+        
+        // Act
+        var action = async () => await SendAsync(query);
+        
+        // Assert
+        await action.Should().ThrowAsync<KeyNotFoundException>()
+            .WithMessage("User group does not exist.");
+    }
+}
