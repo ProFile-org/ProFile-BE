e30bbb3742b730b479fb5f5b3a92f32f7a342c5d
Nguyen Quang Chien <quangchieens1@gmail.com>
2023-06-30 20:39:22 +0700

    update: upload so that an endpoint can both upload a file and create a directory

--- src/Api/Controllers/Payload/Requests/DigitalFile/UploadDigitalFileRequest.cs
+++ src/Api/Controllers/Payload/Requests/DigitalFile/UploadDigitalFileRequest.cs
@@ -5,6 +5,8 @@ namespace Api.Controllers.Payload.Requests.DigitalFile;
 /// </summary>
 public class UploadDigitalFileRequest
 {
-    public string Directory { get; set; } = null!;
-    public IFormFile File { get; set; } = null!;
+    public string Name { get; set; } = null!;
+    public string Path { get; set; } = null!;
+    public bool IsDirectory { get; set; }
+    public IFormFile? File { get; set; }
 }
--- src/Application/Digital/Commands/UploadDigitalFile.cs
+++ src/Application/Digital/Commands/UploadDigitalFile.cs
@@ -17,8 +17,10 @@ public class UploadDigitalFile {
         public Validator()
         {
             RuleLevelCascadeMode = CascadeMode.Stop;
+            
             RuleFor(x => x.Name)
                 .MaximumLength(256).WithMessage("Name cannot exceed 256 characters.");
+            
             RuleFor(x => x.Path)
                 .NotEmpty().WithMessage("File's path is required.")
                 .Matches("^(/(?!/)[a-z_.\\-0-9]*)+(?<!/)$|^/$").WithMessage("Invalid path format.");
@@ -30,8 +32,10 @@ public class UploadDigitalFile {
         public User CurrentUser { get; init; } = null!;
         public string Name { get; init; } = null!;
         public string Path { get; init; } = null!;
-        public MemoryStream FileData { get; init; } = null!;
-        public string FileType { get; init; } = null!;
+        public bool IsDirectory { get; init; }
+        public MemoryStream? FileData { get; init; }
+        public string? FileType { get; init; }
+        public string? FileExtension { get; init; }
     }
     
     public class Handler : IRequestHandler<Command, EntryDto>
@@ -49,53 +53,63 @@ public class UploadDigitalFile {
         
         public async Task<EntryDto> Handle(Command request, CancellationToken cancellationToken)
         {
-            var fileEntry = await _context.Entries.FirstOrDefaultAsync(
-                x => x.Name.Trim().Equals(request.Name.Trim())
-                && x.Path.Trim().Equals(request.Path.Trim()), cancellationToken);
-            
-            if (fileEntry is not null)
-            {
-                throw new ConflictException("File already exists.");
-            }
-
-            var directoryExists = await _context.Entries.AnyAsync(
+            var baseDirectoryExists = await _context.Entries.AnyAsync(
                 x => request.Path.Trim().ToLower().Equals((x.Path + x.Name).ToLower())
                      && x.FileId == null, cancellationToken);
 
-            if (!request.Path.Equals("/") && !directoryExists)
+            if (!request.Path.Equals("/") && !baseDirectoryExists)
             {
-                throw new ConflictException("Directory does not exist.");
+                throw new ConflictException("Base directory does not exist.");
             }
-            
-            // Make this dynamic
-            if (request.FileData.Length > 20971520)
-            {
-                throw new ConflictException("File size must be lower than 20MB");
-            }
-            
+
             var localDateTimeNow = LocalDateTime.FromDateTime(_dateTimeProvider.DateTimeNow);
 
-            var lastDotIndex = request.Name.LastIndexOf(".", StringComparison.Ordinal);
-            var fileExtension = request.Name.Substring(lastDotIndex + 1, request.Name.Length - lastDotIndex - 1);
-            
-            var fileEntity = new FileEntity()
-            {
-                FileData = request.FileData.ToArray(),
-                FileType = request.FileType,
-                FileExtension = fileExtension,
-            };
-            
             var entryEntity = new Entry()
             {
                 Name = request.Name.Trim(),
                 Path = request.Path.Trim(),
-                File = fileEntity,
                 CreatedBy = request.CurrentUser.Id,
                 Uploader = request.CurrentUser,
                 Created = localDateTimeNow,
+                Owner = request.CurrentUser,
+                OwnerId = request.CurrentUser.Id,
             };
             
-            await _context.Files.AddAsync(fileEntity, cancellationToken);
+            if (request.IsDirectory)
+            {
+                var entry = await _context.Entries.FirstOrDefaultAsync(
+                    x => x.Name.Trim().Equals(request.Name.Trim())
+                         && x.Path.Trim().Equals(request.Path.Trim())
+                    && x.FileId == null, cancellationToken);
+            
+                if (entry is not null)
+                {
+                    throw new ConflictException("Directory already exists.");
+                }
+            }
+            else
+            {
+                // Make this dynamic
+                if (request.FileData!.Length > 20971520)
+                {
+                    throw new ConflictException("File size must be lower than 20MB");
+                }
+
+                var lastDotIndex = request.Name.LastIndexOf(".", StringComparison.Ordinal);
+                var fileExtension = request.Name.Substring(lastDotIndex + 1, request.Name.Length - lastDotIndex - 1);
+            
+                var fileEntity = new FileEntity()
+                {
+                    FileData = request.FileData.ToArray(),
+                    FileType = request.FileType!,
+                    FileExtension = request.FileExtension,
+                };
+                entryEntity.FileId = fileEntity.Id;
+                entryEntity.File = fileEntity;
+                
+                await _context.Files.AddAsync(fileEntity, cancellationToken);
+            }
+            
             var result = await _context.Entries.AddAsync(entryEntity, cancellationToken);
             await _context.SaveChangesAsync(cancellationToken);
             
