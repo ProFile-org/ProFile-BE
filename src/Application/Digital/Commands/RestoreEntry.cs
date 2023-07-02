1873cb0428407b0f9c2abf38f593f6251a742134
StarryFolf <dangbao2003lqd@gmail.com>
2023-07-02 12:02:50 +0700

    feat: implement restoring entry

---
+++ src/Application/Digital/Commands/RestoreEntry.cs
@@ -0,0 +1,64 @@
+﻿using Application.Common.Exceptions;
+using Application.Common.Interfaces;
+using Application.Common.Models;
+using Application.Common.Models.Dtos.Digital;
+using AutoMapper;
+using Domain.Entities;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using NodaTime;
+
+namespace Application.Digital.Commands;
+
+public class RestoreEntry
+{
+    public record Command : IRequest<EntryDto>
+    {
+        public User CurrentUser { get; init; } = null!;
+        public Guid EntryId { get; init; }
+    }
+
+    public class Handler : IRequestHandler<Command, EntryDto>
+    {
+        private readonly IApplicationDbContext _context;
+        private readonly IMapper _mapper;
+        private readonly IDateTimeProvider _dateTimeProvider;
+        private const string BinString = "_bin";
+        
+        public Handler(IApplicationDbContext context, IMapper mapper, IDateTimeProvider dateTimeProvider)
+        {
+            _context = context;
+            _mapper = mapper;
+            _dateTimeProvider = dateTimeProvider;
+        }
+
+        public async Task<EntryDto> Handle(Command request, CancellationToken cancellationToken)
+        {
+            var entry = await _context.Entries.FirstOrDefaultAsync(x => x.Id == request.EntryId, cancellationToken);
+
+            if (entry is null)
+            {
+                throw new KeyNotFoundException("Entry does not exist");
+            }
+
+            var path = entry.Path;
+            var firstSlashIndex = path.IndexOf("/", StringComparison.Ordinal);
+            var binCheck = path.Substring(0, firstSlashIndex);
+
+            if (!binCheck.Contains(BinString))
+            {
+                throw new ConflictException("Entry is not in bin");
+            }
+
+            var localDateTimeNow = LocalDateTime.FromDateTime(_dateTimeProvider.DateTimeNow);
+            
+            entry.Path = path.Replace(binCheck, "");
+            entry.LastModified = localDateTimeNow;
+            entry.LastModifiedBy = request.CurrentUser.Id;
+
+            var result = _context.Entries.Update(entry);
+            await _context.SaveChangesAsync(cancellationToken);
+            return _mapper.Map<EntryDto>(result.Entity);
+        }
+    }
+}
