fe32b9301c909b6c42cbe6f78226fbb900b7e688
StarryFolf <dangbao2003lqd@gmail.com>
2023-07-02 16:33:21 +0700

    feat: implement deleting entry in bin

---
+++ src/Application/Digital/Commands/DeleteEntry.cs
@@ -0,0 +1,78 @@
+﻿using Application.Common.Exceptions;
+using Application.Common.Interfaces;
+using Application.Common.Models.Dtos.Digital;
+using AutoMapper;
+using Domain.Entities;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using NodaTime;
+
+namespace Application.Digital.Commands;
+
+public class DeleteEntry
+{
+    public record Command : IRequest<EntryDto>
+    {
+        public User CurrentUser { get; init; } = null!;
+        public Guid EntryId { get; init; }
+    }
+
+    public class Handler : IRequestHandler<Command, EntryDto>
+    {
+        private readonly IApplicationDbContext _context;
+        private readonly IMapper _mapper;
+        private readonly IDateTimeProvider _dateTimeProvider;
+        private const string BinString = "_bin";
+
+        public Handler(IMapper mapper, IApplicationDbContext context, IDateTimeProvider dateTimeProvider)
+        {
+            _mapper = mapper;
+            _context = context;
+            _dateTimeProvider = dateTimeProvider;
+        }
+
+        public async Task<EntryDto> Handle(Command request, CancellationToken cancellationToken)
+        {
+            var entry = await _context.Entries
+                .Include(x=> x.Owner)
+                .FirstOrDefaultAsync(x => x.Id == request.EntryId, cancellationToken);
+
+            if (entry is null)
+            {
+                throw new KeyNotFoundException("Entry does not exist.");
+            }
+
+            var entryPath = entry.Path;
+            var firstSlashIndex = entryPath.IndexOf("/", StringComparison.Ordinal);
+            var binCheck = entryPath.Substring(0, firstSlashIndex);
+
+            if (!binCheck.Contains(BinString))
+            {
+                throw new NotChangedException("Entry is not in bin.");
+            }
+            
+            if (!binCheck.Contains(request.CurrentUser.Username))
+            {
+                throw new NotAllowedException("You do not have the permission to delete this entry.");
+            }
+            
+            if (entry.IsDirectory)
+            {
+                var path = entry.Path[^1].Equals('/') ? entry.Path + entry.Name : $"{entry.Path}/{entry.Name}";
+                var pattern = $"{path}/%";
+                var childEntries = _context.Entries.Where(x => 
+                    x.Path.Trim().ToLower().Equals(path)
+                    || EF.Functions.Like(x.Path, pattern));
+
+                foreach (var childEntry in childEntries)
+                {
+                    _context.Entries.Remove(childEntry);
+                }
+            }
+
+            var result = _context.Entries.Remove(entry);
+            await _context.SaveChangesAsync(cancellationToken);
+            return _mapper.Map<EntryDto>(result.Entity);
+        }
+    }
+}
