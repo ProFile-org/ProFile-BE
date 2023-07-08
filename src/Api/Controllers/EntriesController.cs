229655ac7b699431db5a1bc3f21192a321c70396
Vzart <85790072+Vzart@users.noreply.github.com>
2023-07-08 11:35:06 +0700

    feat: get entry by id (#327)
    * feat: get entry by id
    
    * i am stoopid
    
    * im stoopid 2

--- src/Api/Controllers/EntriesController.cs
+++ src/Api/Controllers/EntriesController.cs
@@ -4,6 +4,7 @@ using Application.Common.Interfaces;
 using Application.Common.Models;
 using Application.Common.Models.Dtos.Digital;
 using Application.Digital.Commands;
+using Application.Digital.Queries;
 using Application.Identity;
 using FluentValidation.Results;
 using Infrastructure.Identity.Authorization;
@@ -89,6 +90,25 @@ public class EntriesController  : ApiControllerBase
     /// 
     /// </summary>
     /// <param name="entryId"></param>
+    /// <returns></returns>
+    [RequiresRole(IdentityData.Roles.Employee)]
+    [HttpGet("{entryId:guid}")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public async Task<ActionResult<Result<EntryDto>>> GetById([FromRoute] Guid entryId)
+    {
+        var query = new GetEntryById.Query()
+        {
+            EntryId = entryId
+        };
+
+        var result = await Mediator.Send(query);
+        return Ok(Result<EntryDto>.Succeed(result));
+    }
+    
+    /// <summary>
+    /// 
+    /// </summary>
     /// <param name="request"></param>
     /// <returns></returns>
     [RequiresRole(IdentityData.Roles.Employee)]
