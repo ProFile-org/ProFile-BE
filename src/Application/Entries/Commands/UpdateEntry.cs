bef066ae18db1459f0db0b9b01b0c95c0d313035
Nguyen Quang Chien <quangchieens1@gmail.com>
2023-07-12 00:39:37 +0700

    update entry now update its child too

--- src/Application/Entries/Commands/UpdateEntry.cs
+++ src/Application/Entries/Commands/UpdateEntry.cs
@@ -87,11 +87,40 @@ public class UpdateEntry
 
             var localDateTimeNow = LocalDateTime.FromDateTime(_dateTimeProvider.DateTimeNow);
             
+            var oldPath = entry.Path.Equals("/") ? entry.Path + entry.Name : $"{entry.Path}/{entry.Name}";
+
             entry.Name = request.Name;
             entry.LastModified = localDateTimeNow;
             entry.LastModifiedBy = request.CurrentUserId;
 
             var result = _context.Entries.Update(entry);
+            
+            if (entry.IsDirectory)
+            {
+                // Update permissions for child Entries
+                var newPath = entry.Path.Equals("/") ? entry.Path + entry.Name : $"{entry.Path}/{entry.Name}";
+                var pattern = $"{oldPath}/%";
+                var childEntries = _context.Entries
+                    .Where(x => (x.Path.Equals(oldPath) || EF.Functions.Like(x.Path, pattern))
+                                && x.OwnerId == entry.OwnerId)
+                    .ToList();
+                var count = oldPath.Length;
+                foreach (var childEntry in childEntries)
+                {
+                    if (childEntry.Path.Equals(oldPath))
+                    {
+                        childEntry.Path = newPath;
+                    }
+                    else
+                    {
+                        var after = childEntry.Path[(count+1)..];
+                        childEntry.Path = $"{newPath}/{after}";
+                    }
+                }
+
+                _context.Entries.UpdateRange(childEntries);
+            }
+            
             await _context.SaveChangesAsync(cancellationToken);
             using (Logging.PushProperties(nameof(Entry), entry.Id, request.CurrentUserId))
             {
