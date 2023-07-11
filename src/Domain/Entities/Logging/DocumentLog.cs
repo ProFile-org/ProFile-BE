f7cce3e7a944bf763319570eedc21271b282b7e3
Nguyen Quang Chien <quangchieens1@gmail.com>
2023-07-11 14:40:20 +0700

    delete: all logs entities

--- src/Domain/Entities/Logging/DocumentLog.cs
+++
@@ -1,9 +0,0 @@
-using Domain.Common;
-using Domain.Entities.Physical;
-
-namespace Domain.Entities.Logging;
-
-public class DocumentLog : BaseLoggingEntity
-{
-    public Folder? BaseFolder { get; set; }
-}
