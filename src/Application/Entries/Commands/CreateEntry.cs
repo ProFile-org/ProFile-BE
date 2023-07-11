cb21d9816d7afe14720727d8c23f59a84c4f00f6
StarryFolf <67864500+StarryFolf@users.noreply.github.com>
2023-07-11 19:27:05 +0700

    fix: regex validation and change command name. (#366)
    * fix: regex validation and change command name.
    
    * refactoring

--- src/Application/Entries/Commands/CreateEntry.cs
+++ src/Application/Entries/Commands/CreateEntry.cs
@@ -1,8 +1,8 @@
-﻿using Application.Common.Exceptions;
+using Application.Common.Exceptions;
 using Application.Common.Interfaces;
 using Application.Common.Logging;
 using Application.Common.Models.Dtos.Digital;
-using Application.Common.Models.Operations;
+using Application.Helpers;
 using AutoMapper;
 using Domain.Entities;
 using Domain.Entities.Digital;
@@ -14,8 +14,7 @@ using NodaTime;
 
 namespace Application.Entries.Commands;
 
-public class UploadSharedEntry
-{
+public class CreateEntry {
     public class Validator : AbstractValidator<Command>
     {
         public Validator()
@@ -23,80 +22,74 @@ public class UploadSharedEntry
             RuleLevelCascadeMode = CascadeMode.Stop;
             
             RuleFor(x => x.Name)
+                .NotEmpty().WithMessage("Entry's name is required.")
                 .MaximumLength(256).WithMessage("Name cannot exceed 256 characters.");
+            
+            RuleFor(x => x.Path)
+                .NotEmpty().WithMessage("Entry's path is required.")
+                .Matches("^(/(?!/)[a-z_.\\s\\-0-9]*)+(?<!/)$|^/$").WithMessage("Invalid path format.");
         }
     }
+    
     public record Command : IRequest<EntryDto>
     {
-        public Guid EntryId { get; init; }
         public User CurrentUser { get; init; } = null!;
         public string Name { get; init; } = null!;
+        public string Path { get; init; } = null!;
         public bool IsDirectory { get; init; }
         public MemoryStream? FileData { get; init; }
         public string? FileType { get; init; }
         public string? FileExtension { get; init; }
     }
     
-    public class CommandHandler : IRequestHandler<Command, EntryDto>
+    public class Handler : IRequestHandler<Command, EntryDto>
     {
         private readonly IApplicationDbContext _context;
-        private readonly IMapper _mapper;
+        private readonly IMapper _mapper; 
         private readonly IDateTimeProvider _dateTimeProvider;
-        private readonly ILogger<UploadSharedEntry> _logger;
-        public CommandHandler(IApplicationDbContext context, IMapper mapper, IDateTimeProvider dateTimeProvider, ILogger<UploadSharedEntry> logger)
+        private readonly ILogger<CreateEntry> _logger;
+
+        public Handler(IApplicationDbContext context, IMapper mapper, IDateTimeProvider dateTimeProvider, ILogger<CreateEntry> logger)
         {
             _context = context;
             _mapper = mapper;
             _dateTimeProvider = dateTimeProvider;
             _logger = logger;
         }
-
+        
         public async Task<EntryDto> Handle(Command request, CancellationToken cancellationToken)
         {
-            var permissions = _context.EntryPermissions
-                .Include(x => x.Entry)
-                .ThenInclude(y => y.Uploader)
-                .Include(x => x.Entry)
-                .ThenInclude(x => x.Owner)
-                .Where(x => x.EmployeeId == request.CurrentUser.Id 
-                            && x.AllowedOperations.Contains(EntryOperation.Upload.ToString()));
+            var baseDirectoryExists = await _context.Entries.AnyAsync(
+                x => request.Path.Trim().ToLower()
+                         .Equals((x.Path.Equals("/") ? (x.Path + x.Name) : (x.Path + "/" + x.Name)).ToLower())
+                     && x.FileId == null, cancellationToken);
 
-            var rootEntry = await permissions
-                .Select(x => x.Entry)
-                .FirstOrDefaultAsync(x => x.Id == request.EntryId, cancellationToken);
-
-            if (rootEntry is null)
+            if (!request.Path.Equals("/") && !baseDirectoryExists)
             {
-                throw new KeyNotFoundException("Entry does not exist.");
+                throw new ConflictException("Base directory does not exist.");
             }
 
-            if (!rootEntry.IsDirectory)
-            {
-                throw new ConflictException("This is a file.");
-            }
-            
             var localDateTimeNow = LocalDateTime.FromDateTime(_dateTimeProvider.DateTimeNow);
-            var entryPath = rootEntry.Path + "/" + request.Name.Trim();
-            
-            var entity = new Entry()
+
+            var entryEntity = new Entry()
             {
                 Name = request.Name.Trim(),
-                Path = entryPath,
+                Path = request.Path.Trim(),
                 CreatedBy = request.CurrentUser.Id,
                 Uploader = request.CurrentUser,
                 Created = localDateTimeNow,
-                Owner = rootEntry.Owner,
-                OwnerId = rootEntry.Owner.Id,
+                Owner = request.CurrentUser,
+                OwnerId = request.CurrentUser.Id,
+                SizeInBytes = null
             };
-
+            
             if (request.IsDirectory)
             {
-                var entry = await _context.Entries
-                    .FirstOrDefaultAsync(
-                        x => x.Name.Trim().Equals(request.Name.Trim()) 
-                             && x.Path.Equals(entryPath) 
-                             && x.FileId == null,cancellationToken);
-                
+                var entry = await _context.Entries.FirstOrDefaultAsync(
+                    x => x.Name.Trim().Equals(request.Name.Trim())
+                         && x.Path.Trim().Equals(request.Path.Trim())
+                    && x.FileId == null, cancellationToken);
+            
                 if (entry is not null)
                 {
                     throw new ConflictException("Directory already exists.");
@@ -104,11 +97,12 @@ public class UploadSharedEntry
             }
             else
             {
-                if (request.FileData!.Length > 20971520)
+                // Make this dynamic
+                if (request.FileData!.Length > FileUtil.ToByteFromMb(20))
                 {
                     throw new ConflictException("File size must be lower than 20MB");
                 }
-                
+
                 var lastDotIndex = request.Name.LastIndexOf(".", StringComparison.Ordinal);
                 var fileExtension = request.Name.Substring(lastDotIndex + 1, request.Name.Length - lastDotIndex - 1);
             
@@ -118,18 +112,18 @@ public class UploadSharedEntry
                     FileType = request.FileType!,
                     FileExtension = request.FileExtension,
                 };
+                entryEntity.FileId = fileEntity.Id;
+                entryEntity.File = fileEntity;
+                entryEntity.SizeInBytes = request.FileData!.Length;
                 
                 await _context.Files.AddAsync(fileEntity, cancellationToken);
-
-                entity.FileId = fileEntity.Id;
-                entity.File = fileEntity;
             }
             
-            var result = await _context.Entries.AddAsync(entity, cancellationToken);
+            var result = await _context.Entries.AddAsync(entryEntity, cancellationToken);
             await _context.SaveChangesAsync(cancellationToken);
-            using (Logging.PushProperties(nameof(Entry), entity.Id, request.CurrentUser.Id))
+            using (Logging.PushProperties(nameof(Entry), entryEntity.Id, request.CurrentUser.Id))
             {
-                _logger.LogUploadSharedEntry(request.CurrentUser.Id.ToString(), entity.Id.ToString());
+                _logger.LogCreateEntry(request.CurrentUser.Id.ToString(), entryEntity.Id.ToString());
             }
             return _mapper.Map<EntryDto>(result.Entity);
         }
