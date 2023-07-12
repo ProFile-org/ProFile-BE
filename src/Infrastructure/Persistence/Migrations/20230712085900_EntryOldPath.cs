ec4388f049c02bd90ed029fdf66995b7d2210a8e
Nguyen Quang Chien <quangchieens1@gmail.com>
2023-07-12 21:34:38 +0700

    I forgot to ship this

---
+++ src/Infrastructure/Persistence/Migrations/20230712085900_EntryOldPath.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class EntryOldPath : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "OldPath",
+                table: "Entries",
+                type: "text",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "OldPath",
+                table: "Entries");
+        }
+    }
+}
