e8fbab2721720dea6ccc1977c58eab99dd3bac43
Vzart <vo.dang.khoa.main@gmail.com>
2023-07-28 22:20:40 +0700

    add: metric for import document count

--- src/Api/Controllers/DashboardController.cs
+++ src/Api/Controllers/DashboardController.cs
@@ -1,12 +1,33 @@
+using Api.Controllers.Payload.Requests.Dashboard;
 using Application.Common.Interfaces;
+using Application.Common.Models;
+using Application.Dashboards.Queries;
+using Application.Identity;
+using Infrastructure.Identity.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Api.Controllers;
 
 public class DashboardController : ApiControllerBase
 {
     private readonly ICurrentUserService _currentUserService;
+    
     public DashboardController(ICurrentUserService currentUserService)
     {
         _currentUserService = currentUserService;
+    }
+
+    [RequiresRole(IdentityData.Roles.Admin)]
+    [HttpPost("import-documents")]
+    public async Task<ActionResult<Result<List<GetImportDocuments.Result>>>> GetImportDocuments(GetImportedDocumentsMetricsRequest request)
+    {
+        var query = new GetImportDocuments.Query()
+        {
+            StartDate = request.StartDate,
+            EndDate = request.EndDate
+        };
+
+        var result = await Mediator.Send(query);
+        return Ok(Result<List<GetImportDocuments.Result>>.Succeed(result));
     }
 }
---
+++ src/Application/Dashboards/Queries/GetImportDocuments.cs
@@ -0,0 +1,81 @@
+﻿using Application.Common.Interfaces;
+using Domain.Statuses;
+using FluentValidation;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using NodaTime;
+
+namespace Application.Dashboards.Queries;
+
+public class GetImportDocuments
+{
+    public class Validator : AbstractValidator<Query>
+    {
+        public Validator()
+        {
+            RuleLevelCascadeMode = CascadeMode.Stop;
+
+            RuleFor(x => x.StartDate)
+                .NotEmpty().WithMessage("Start date can not be empty.");
+
+            RuleFor(x => x.EndDate)
+                .NotEmpty().WithMessage("End date can not be empty.");
+        }
+    }
+    public class Result
+    {
+        public string Label { get; set; }
+        public int Value { get; set; }
+    }
+
+    public record Query : IRequest<List<Result>>
+    {
+        public DateTime StartDate { get; init; }
+        public DateTime EndDate { get; init; }
+
+    }
+
+    public class QueryHandler : IRequestHandler<Query, List<Result>>
+    {
+        private readonly IApplicationDbContext _context;
+        private readonly IDateTimeProvider _dateTimeProvider;
+
+        public QueryHandler(IApplicationDbContext context, IDateTimeProvider dateTimeProvider)
+        {
+            _context = context;
+            _dateTimeProvider = dateTimeProvider;
+        }
+
+        public async Task<List<Result>> Handle(Query request, CancellationToken cancellationToken)
+        {
+            var result = new List<Result>();
+
+            var currentDate = request.StartDate.Date;
+            var endDate = request.EndDate.Date;
+
+            while (currentDate <= endDate)
+            {
+                var nextDate = currentDate.AddDays(1);
+                var label = $"{currentDate:dd-MM} to {nextDate:dd-MM}";
+                if (nextDate <= endDate)
+                {
+                    var date = currentDate;
+                    var importedDocumentsCount = await _context.Documents
+                        .Where(x => x.Created >= LocalDateTime.FromDateTime(date)
+                                    && x.Created <= LocalDateTime.FromDateTime(nextDate)
+                                    && x.Status != DocumentStatus.Issued)
+                        .CountAsync(cancellationToken);
+                
+                    result.Add(new Result()
+                    {
+                        Label = label,
+                        Value = importedDocumentsCount
+                    });
+                }
+                currentDate = nextDate;
+            }
+
+            return result;
+        }
+    }
+}
